5704d2d1f1ab5f0f0ee0a9390fd96dbd8f121ecd
anoushk1234 <anoushka1828@gmail.com>
2023-03-24 14:14:49 +0530

    tests: added testing script

---
+++ scripts/test-cli.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+echo "\033[0;32mstarting rpc connection(might error if already connected)..."
+./rpc
+echo "\033[0;32mbuilding cli..."
+sh path
+
+tinydancer
+
+read cont
+if [ "$cont" != "y" ]; then
+    exit 1
+fi
+
+tput setaf 2; echo "testing config..."
+tinydancer config get
+tinydancer config set --cluster Localnet --log-path /tmp/client.logPath
+# tinydancer config get
+
+echo "Continue? (y/n)"
+read cont
+if [ "$cont" != "y" ]; then
+    exit 1
+fi
+
+echo "\033[0;32mtesting slot..."
+slot=$(tinydancer slot)
+slot_number=$(echo $slot | cut -d':' -f 2 | xargs | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
+sleep 1
+tinydancer verify --slot "$slot_number"
+
+echo "Continue? (y/n)"
+read cont
+if [ "$cont" != "y" ]; then
+    exit 1
+# fi
+
+echo "\033[0;32mtesting daemon..."
+# should handle gracefully
+tinydancer start
+sleep 1
+tinydancer start "/tmp"
+PID=$! 
+kill -INT $PID # kill if still running
+
+echo "Continue? (y/n)"
+read cont
+if [ "$cont" != "y" ]; then
+    exit 1
+fi
+
+
+echo "\033[0;32mtests complete"
